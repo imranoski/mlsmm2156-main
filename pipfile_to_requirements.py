8f8bfc80f71493dd1649c1399880acd7a0df71d7
imranoski <imrane.janati@student.uclouvain.be>
2025-04-21 15:23:08 +0200

    adjust_piprequire

--- pipfile_to_requirements.py
+++ pipfile_to_requirements.py
@@ -3,13 +3,14 @@ import configparser
 
 def main():
     parser = configparser.ConfigParser()
-    parser.read("recommender_systems/mlsmm2156-main/Pipfile")
+    parser.read("Pipfile")
 
     packages = "packages"
-    with open("recommender_systems/mlsmm2156-main/requirements.txt", "w") as f:
+    with open("requirements.txt", "w") as f:
         for key in parser[packages]:
             value = parser[packages][key]
-            f.write(key + value.replace("\"", "") + "\n")
-
+            f.write(key + "\n")
+            if '"*"' in value:
+                value.replace('"*"', "")
 
 main()
